f03d41d757c7a17bc60d613a678f0d34913fe574
rymzx <rmasihin7@gmail.com>
2022-03-03 14:03:18 +0700

    validasi selesai

--- script.js
+++ script.js
@@ -18,6 +18,18 @@ const result = document.getElementById('result');
 addButton.addEventListener('click', () => {
   const inputOneValue = inputOne.value;
   const inputTwoValue = inputTwo.value;
-  const addedResult = parseInt(inputOneValue) + parseInt(inputTwoValue);
-  result.innerHTML = addedResult;
+  if (validation(inputOneValue, inputTwoValue)) {
+    const addedResult = parseInt(inputOneValue) + parseInt(inputTwoValue);
+    result.innerHTML = addedResult;
+  } else {
+    alert('input tidak valid');
+  }
 });
+
+function validation(inputOne, inputTwo) {
+  if (inputOne == false || inputTwo == false || isNaN(inputOne) || isNaN(inputTwo)) {
+    return false;
+  } else {
+    return true;
+  }
+}
